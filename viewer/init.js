662a0578f47cea3bb31a56c58cde9376bfbf6b11
Fabian <bart@macMini.local>
2020-06-20 09:06:46 +0200

    Viewer updated

--- viewer/init.js
+++ viewer/init.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     new ArchivalIIIFViewer({
         id: 'root',
-        language: 'en',
         manifest: window.location.href + 'iiif/collection/demo'
     });
 });

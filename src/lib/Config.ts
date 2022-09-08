d61dfda3e0dd90b8b50aec7f689df723813ccfb5
Fabian <edv@sozarch.uzh.ch>
2022-09-08 17:54:46 +0200

    ts strict fix

--- src/lib/Config.ts
+++ src/lib/Config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 module.exports = {
 
     port: (() => {
-        const port = parseInt(process.env.PORT);
+        const port = parseInt(process.env.PORT ?? '3333');
         return (port >= 0) ? port : 3333;
     })()
 };
